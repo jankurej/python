b23fd09cd70500792bc22c9dedb849def102f7d7
Johny Smirnof <johny.smirnof@gmail.com>
2017-04-05 14:22:11 +0200

    Operators and Operands.py

---
+++ Operators and Operands.py
@@ -0,0 +1,16 @@
+#Operators and Operands
+20 + 32
+5 ** 2
+(5 + 9) * (15 - 7)
+print 7 + 5
+
+print 9 / 5
+print 9.0 / 5
+print 9 / 5.0
+print 5/9
+
+print 7.0 / 3.0
+print 7.0 // 3.0
+
+print 7 // 3     # This is the integer division operator
+print 7 % 3      # This is the remainder or modulus operator
