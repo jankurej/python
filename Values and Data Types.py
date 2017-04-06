aeabe345a727f0e661873e136eafc2b545dc02e7
Johny Smirnof <johny.smirnof@gmail.com>
2017-04-06 12:33:10 +0200

    Values and Data Types.py

---
+++ Values and Data Types.py
@@ -0,0 +1,12 @@
+#A value is one of the fundamental things - like a word or a number - that a program manipulates. Example of value is 5 (result when we added 2+3), and "Hello, World".
+#We can specifiy values directly in the programs we write. For example we can specify a number as a literal just by writing it directly, (e.g. 5 or 4.32). In a program, we specify a word, or more generally a string of characters, by enclosing the characters inside quotation marks (e.g., "Hello, World").
+#During execution of a programm the python interpreter creates an internal representation of values that are specified in a program.
+#It can then manipulate them.
+#For example, in the next section, we'll look at how a program can specify that values should be combined using operators like + and *. We call the internal representations objects or just values.
+
+print 3.2
+print "Hello, World!"
+
+#Special characters, like the tab character \t and the newline character \n whose internal and external representations are different.
+
+print "Two tabs after this\tand then newlines\n\nand that's all"
